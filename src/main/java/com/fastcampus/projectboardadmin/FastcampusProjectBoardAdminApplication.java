a11ec76136b6511e0b51f16920ee82d9ca2bbf6f
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-10-17 09:18:32 +0900

    Initial commit

---
+++ src/main/java/com/fastcampus/projectboardadmin/FastcampusProjectBoardAdminApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.projectboardadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusProjectBoardAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastcampusProjectBoardAdminApplication.class, args);
+	}
+
+}
